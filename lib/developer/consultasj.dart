a3bf4ef7e67c6bc1c98480361f567de1e830526f
JosueAlbanez3 <josuealbanez05gmail.com>
2023-06-08 10:38:09 -0600

    bol

---
+++ lib/developer/consultasj.dart
@@ -0,0 +1,22 @@
+import 'package:http/http.dart' as http;
+
+import 'dart:async';
+//Libreria que nos permitira decodificar los archivos JSON
+import 'dart:convert';
+//Libreria que nos permitira usar Colecciones (array y MAP)
+// ignore: unused_import
+import 'dart:collection';
+
+// ignore: unused_import
+import 'dart:io';
+
+Future<List<dynamic>> fetchUserData() async {
+  final response = await http.get(Uri.parse('https://notasincas.000webhostapp.com/llamarduidatosestu.php'));
+  if (response.statusCode == 200) {
+    // Decodificar la respuesta JSON
+    final jsonData = json.decode(response.body);
+    return jsonData;
+  } else {
+    throw Exception('Error al cargar los datos');
+  }
+}
