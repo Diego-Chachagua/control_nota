012c568a10670ac9221fc2effaf9c0aa440f38a4
JosueAlbanez3 <josuealbanez05gmail.com>
2023-06-08 18:46:05 -0600

    cambio consultasj

--- lib/developer/consultasj.dart
+++ lib/developer/consultasj.dart
@@ -11,7 +11,7 @@ import 'dart:collection';
 import 'dart:io';
 
 Future<List<dynamic>> fetchUserData() async {
-  final response = await http.get(Uri.parse('https://notasincas.000webhostapp.com/llamarduidatosestu.php'));
+  final response = await http.get(Uri.parse('https://notasincas.000webhostapp.com/boleta1.php'));
   if (response.statusCode == 200) {
     final jsonData = json.decode(response.body);
     return jsonData as List<dynamic>;
