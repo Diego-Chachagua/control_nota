687d5bf0bc08916d53af60d3a18078b238c7de8e
jeremias-2005 <1907791@clases.edu.sv>
2023-06-16 20:43:01 -0600

    cambios regisro profesor

--- lib/developer/consultase.dart
+++ lib/developer/consultase.dart
@@ -10,12 +10,16 @@ import 'dart:collection';
 // ignore: unused_import
 import 'dart:io';
 
-Future<dynamic> registroProfesor(String nombre1, String apellido1, String anio, String genero1, var m1, var m2, var m3, var m4, var m5, var m6, var m7, var m8, var m9, var m10) async{
+Future<dynamic> registroProfesor(String nombre1, String secciones, String apellido1, String anio, String genero1, var m1, var m2, var m3, var m4, var m5, var m6, var m7, var m8, var m9, var m10, var primero, var segundo) async{
   http.Response Guardar = await http.post(
   Uri.parse('https://notasincas.000webhostapp.com/registro_profe.php'),
   body: <String, dynamic>{
+    "primero" : primero,
+    "segundo" : segundo,
+    "secciones": secciones,
     "nombre1": nombre1,
     "apellido1": apellido1,
+    "anio": anio,
     "genero1": genero1,
     "m1": m1,
     "m2": m2,
@@ -30,7 +34,11 @@ Future<dynamic> registroProfesor(String nombre1, String apellido1, String anio, 
     
   }
   );
-  
+  print(secciones);
+  print(secciones);
+  print(secciones);
+  print(secciones);
+  print(secciones);
   print(nombre1);
   print(apellido1);
   print(anio);
@@ -45,5 +53,11 @@ Future<dynamic> registroProfesor(String nombre1, String apellido1, String anio, 
   print(m8);
   print(m9);
   print(m10);
+  if (Guardar.statusCode == 201) {
+    return "error";
+  } else {
+
+    return Guardar.body;
+  }
 }
 
